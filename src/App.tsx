91e48a64af50824a884db085622a96bb92ee5dd7
Babin Bohora <bohorababin1@gmail.com>
2022-05-11 14:48:37 +0900

    logic done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './App.css';
 import Row from './components/Row';
 
@@ -22,21 +21,78 @@ export const useHandleClick = (): handleClickType => {
 
 
 function App() {
-  // const [isX, setIsX] = React.useState<boolean>(false);
+  const [isX, setIsX] = React.useState<boolean>(false);
+  
+  function restartGame(tdes: NodeListOf<HTMLElement>) {
+    tdes.forEach((td) => {
+      td.dataset.turn = ""  
+      td.textContent = ""
+      td.style.background = "initial"
+    })
+  };
+  
+
+
+  const gameCheck: handleClickType = (e) => {
+
+    const row: string = e.currentTarget.dataset.row as string;
+    const col: string = e.currentTarget.dataset.col as string; 
+
+    const winOrNot = (boxes: HTMLElement[]): void => {
+      if (boxes.every(td => td.dataset.turn === `${isX}`)) {
+        boxes.forEach((td)=> {td.style.background="green"});
+        const tdes = document.querySelectorAll<HTMLElement>('td');
+        tdes.forEach(td => {
+          if (!td.dataset.turn) td.dataset.turn = "over";
+        })
+        setTimeout( ()=> {
+          if (window.confirm("Would you like to start a new game?")) {
+            restartGame(tdes);
+          }
+        }, 1000)
+      }
+    };
+    // Horizontal Check
+    (() => {
+      const xBoxes = document.querySelectorAll<HTMLElement>(`[data-row='${row}']`);
+      winOrNot(Array.from(xBoxes));
+    })();
+    
+    
+    // Vertical Check
+    (() => {
+      const yBoxes = document.querySelectorAll<HTMLElement>(`[data-col='${col}']`);
+      winOrNot(Array.from(yBoxes));
+    })();
+    // CrossCheck
+    (() => {
+      if (row === col) {
+        const backBoxes: HTMLElement[] = [];
+        for (let i = 1; i <=3; i++) {
+          let box = document.querySelector<HTMLElement>(`[data-row='${i}'][data-col='${i}']`)
+          if (box) backBoxes.push(box);
+        };
+        if (backBoxes.length > 0) winOrNot(backBoxes);
+      }
+      if (['1x3', '2x2', '3x1'].includes(`${row}x${col}`)) {
+        const frontBoxes: HTMLElement[] = [];
+        for (let i = 1; i <=3; i++) {
+          let box = document.querySelector<HTMLElement>(`[data-row='${i}'][data-col='${4 - i}']`)
+          if (box) frontBoxes.push(box);
+        }
+        if (frontBoxes.length > 0) winOrNot(frontBoxes);
+      }
+    })();
+    // Moves Over Check
+  }
+
   const handleClick: handleClickType = (e) => {
-    console.log(e.currentTarget)
+    if (e.currentTarget.dataset.turn) return
+    e.currentTarget.textContent = isX ? "X" : "O";
+    e.currentTarget.dataset.turn = `${isX}`;
+    gameCheck(e);
+    setIsX(() => !isX);
   } 
-
-
-
-
-
-
-
-
-
-
-
 
   // Creating Rows
   const row:React.ReactElement[] = [];
@@ -46,7 +102,8 @@ function App() {
   return (
     <HandleClickContext.Provider value={ handleClick }>
       <div className="App">
-        <h2>0's turn</h2>
+        <button onClick={() => restartGame(document.querySelectorAll<HTMLElement>("td"))}>Restart</button>
+        <h2><span>{isX ? "X" : "O"}</span>'s turn</h2>
         <table>
           <tbody>
             { row }
@@ -58,3 +115,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Box from './Box';
+
 const Row: React.FC<{rowNo: number}> = ({ rowNo }) => {
 
   const boxes: React.ReactElement[] = [];
