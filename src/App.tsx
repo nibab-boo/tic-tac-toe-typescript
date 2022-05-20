dcbd70e3bf2a5b1d85fb174cef01cfd5873e2c99
Babin Bohora <bohorababin1@gmail.com>
2022-05-21 00:11:06 +0900

    roomform created

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ActionCable from 'actioncable';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-// import ActionCable from 'actioncable';
-// import Table from './components/Table'
+
 import OfflineRoom from './components/OfflineRoom'
 import OnlineRoom from './components/OnlineRoom'
+import RoomForm from './components/OnlineRoom'
 // types
 type clickEvent = React.MouseEvent<HTMLTableCellElement>;
 export type handleClickType = (e: clickEvent)=> void 
@@ -25,22 +25,22 @@ function App() {
 
   return (
     <Router>
-      {/* <HandleClickContext.Provider value={ handleClick }> */}
-        <div className="App">
-          {/* <button onClick={() => restartGame(document.querySelectorAll<HTMLElement>("td"))}>Restart</button>
-          <h2><span>{isX ? "X" : "O"}</span>'s turn</h2> */}
-            <Routes>
+      <div className="App">
+          <Routes>
             <Route path="/tic-tac-toe-typescript" element={
               <OfflineRoom />
+            }>
+            </Route>
+            <Route path="/tic-tac-toe-typescript/Rooms" element={
+              <RoomForm />
             }>
             </Route>
             <Route path="/tic-tac-toe-typescript/gameroom/:id/:status" element={
               <OnlineRoom cable= {cable} />
             }>
             </Route>
-            </Routes>
-        </div>
-      {/* </HandleClickContext.Provider> */}
+          </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/components/RoomForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RoomForm = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default RoomForm;
