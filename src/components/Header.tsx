add05ee63b0cd97c94bcd74f489d323e6b3e2693
Babin Bohora <bohorababin1@gmail.com>
2022-05-21 01:10:18 +0900

    Header connected to RoomForm

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { restartGame } from '../App';
+import { useNavigate } from 'react-router-dom';
+
 const Header: React.FC<{ active: string}> = ({ active }) => {
+  const navigate = useNavigate();
+
+  const goOnline = ():void => {
+    navigate("/tic-tac-toe-typescript/rooms");
+  }
+
   return (
     <div className="header">
-      <h2 className={ active === "offline" ? "active" : ""}>Offline Mode</h2>
-      <h2>Online Mode</h2>
-      <button className='reset' onClick={() => restartGame(document.querySelectorAll<HTMLElement>("td"))}>Restart</button>
+      { active !== "roomform" ? 
+      <>
+        <h3 className={ active === "offline" ? "active" : ""}>{active === "offline" ? "Offline Mode" : "Delete Room" }</h3>
+        <h3 onClick={()=> goOnline()} className={ active === "online" ? "active" : ""}>Online Mode</h3>
+        <button className='reset' onClick={() => restartGame(document.querySelectorAll<HTMLElement>("td"))}>Restart</button>
+      </> : <>
+        <h5 className='reset' style={{marginLeft: "0"}}>Go back</h5>
+      </>
+      }
     </div>
   );
 };
--- src/components/RoomForm.tsx
+++ src/components/RoomForm.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from './Header';
 
 const RoomForm = () => {
+  const navigate = useNavigate();
+  const [value, setValue] = React.useState<string>("")
+    
+  const redirectToGameRoom = () => {
+    console.log("join", value);
+    fetch(`https://game-room-center.herokuapp.com/gamerooms/${value.trimEnd()}`)
+      .then((response):void => {
+        console.log(response);
+        if (response.statusText === 'Found') {
+          const redirect = `/tic-tac-toe-typescript/gameroom/${value.trimEnd()}/guest`
+          navigate(redirect);
+        } else {
+          window.alert(`Sorry, we were not able to find ${value.trimEnd()} gameroom. Please, check with your friend or you might want to create a new one.`)
+        }
+      })
+  } 
+  const createGameRoom = () => {
+    console.log("create", value);
+    const formData = new FormData();
+    formData.append("gameroom[name]", value);
+    fetch("https://game-room-center.herokuapp.com/gamerooms", {
+      method: 'post',
+      body: formData
+    }).then(response => response.json())
+    .then((data):void => {
+      console.log(data);
+      if (data.status === "created") {
+        const redirect = `/tic-tac-toe-typescript/gameroom/${data.gameroom_name}/admin`
+        navigate(redirect);
+      } else {
+        window.alert(data.error_msg);
+      }
+    })
+  }
+
   return (
-    <div>
-      
+    <div className="room-form">
+      <Header active="roomform" />
+      <form>
+        <input type={"text"} placeholder="Enter a gameroom" value={value} onChange={(e)=> setValue(e.target.value)}></input>
+        <div>
+          <input type="button" value="Create a gameroom" onClick={(e)=> {
+            e.preventDefault();
+            createGameRoom();
+          }} />
+          <input type="button" value="Enter a gameroom" onClick={(e):void=> { 
+            e.preventDefault();
+            redirectToGameRoom()
+          }} />
+        </div>
+      </form>
     </div>
   );
 };
